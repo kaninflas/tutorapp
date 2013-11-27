8fd14998e2ea2c2a4418f9b18d4e48d9a4198944
a_kanin <a_kanin@hotmail.com>
2013-11-27 12:43:21 -0600

    Comente para agregar usuarios
    Comenté una línea para que te permita agregar usuarios don
    localhost/tutorapp/users/add … esta muy basicota pero para que agregues
    tus usuarios

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends AppController {
     
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('add');
+        //$this->Auth->allow('add');
     }
     
     /*public function login() {
