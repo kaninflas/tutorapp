8ef2ae9b0dd4df1231177b47e5c2310b65913cd0
a_kanin <a_kanin@hotmail.com>
2013-12-05 00:04:41 -0600

    Cambio de direccionamiento inicial
    Para una mayor seguridad se cambio el direccionamiento inicial

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,7 +26,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'Main', 'action' => 'index'));
+	Router::connect('/', array('controller' => 'Users', 'action' => 'login'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
