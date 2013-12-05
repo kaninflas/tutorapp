f71ece2220fea9af9feffe43ce774da7589f6870
a_kanin <a_kanin@hotmail.com>
2013-12-05 00:06:00 -0600

    No recuerdo que cambios
    No recuerdo que cambios hice aqui

--- app/Controller/MainController.php
+++ app/Controller/MainController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @class Controller/MainController
  *
@@ -13,96 +14,101 @@
  * @copyright     Copyright 2013, Novenitos
  * @version       TutorApp v 0.0.1
  */
-
 class MainController extends AppController {
 
 //     /* Modelos que usa la BD */
-	var $uses= array(
-			'User',
-			'Alumnos',
-			'Tutores',
-			'DPA'
-	);
+    var $uses = array(
+        'User',
+        'Alumnos',
+        'Tutores',
+        'DPA'
+    );
 
-  /**
-  * @desc Renderizo la vista principal donde se juntan todas las vistas
-  * @return object    
-  * @access public      
-  */
-  function index(){
-  $this->layout = 'index';
-  }	
-  /**
-  * @desc Renderizo usuarios a la vista principal
-  * @return object    
-  * @access public      
-  */
-  function usuarios(){
-    $registrosUsuario= $this->User->find('all',array('order'=>'id'));	//	almacenamos en una variable la consulta a la tabla users 
-    foreach($registrosUsuario as $key => $value){
-      $arrResp['items'][] = $value['User'];
-    }		
-    print_r(json_encode($arrResp));//hacemos un  encode para enviar los datos en un json
-    exit;
-  }
-  /**
-  * @desc Renderizo alumnos a la vista principal
-  * @return object    
-  * @access public      
-  */
-  function alumnos(){
-    $registrosAlumnos=$this->Alumnos->find('all',array('order'=>'id'));		
-    foreach($registrosAlumnos as $key => $value){
-      $arrResp['items'][] = $value['Alumnos'];
-    }		
-    print_r(json_encode($arrResp));
-    exit;
-  }     
-  /**
-  * @desc Renderizo tutores a la vista principal
-  * @return object    
-  * @access public      
-  */
-  function tutores(){
-    $registrosTutores=$this->Tutores->find('all',array('order'=>'id'));		
-    foreach($registrosTutores as $key => $value){
-      $arrResp['items'][] = $value['Tutores'];
-    }		
-    print_r(json_encode($arrResp));
-    exit;
-  }     
-  /**
-  * @desc Renderizo dpa a la vista principal
-  * @return object    
-  * @access public      
-  */
-  function dpa(){
-    $registrosDPA=$this->DPA->find('all',array('order'=>'id'));		
-    foreach($registrosDPA as $key => $value){
-      $arrResp['items'][] = $value['DPA'];
-    }		
-    print_r(json_encode($arrResp));
-    exit;
-  }
-  function dpa_crear(){
-    $datos = $_POST;
-      $arrDatos = array(
-            'nombre'      => $datos['nombre'],//tomamos el campo directamiente de los datos transferidos atraves el post 
-            'primer_apellido'   => $datos['primer_apellido'],//es importante que el NAME del campo sea EXACTAMENTE IGUAL al que tomaremos del POST
-            'segundo_apellido'  => $datos['segundo_apellido']
-        );  
-      //Aqui guardamos el renglon
-       $this->DPA->create();//creamos un regnlon vacio para
-         $this->DPA->save($arrDatos);//posteriormente guardar ahi los datos del array que generamos
 
-          $respuesta['success'] = true;
-        $respuesta['msg']   = "Datos guardados";
+    /**
+     * @desc Renderizo la vista principal donde se juntan todas las vistas
+     * @return object    
+     * @access public      
+     */
+    function index() {
+        $this->layout = '';
+    }
+
+    /**
+     * @desc Renderizo usuarios a la vista principal
+     * @return object    
+     * @access public      
+     */
+    function usuarios() {
+        $registrosUsuario = $this->User->find('all', array('order' => 'id')); //	almacenamos en una variable la consulta a la tabla users 
+        foreach ($registrosUsuario as $key => $value) {
+            $arrResp['items'][] = $value['User'];
+        }
+        print_r(json_encode($arrResp)); //hacemos un  encode para enviar los datos en un json
+        exit;
+    }
+
+    /**
+     * @desc Renderizo alumnos a la vista principal
+     * @return object    
+     * @access public      
+     */
+    function alumnos() {
+        $registrosAlumnos = $this->Alumnos->find('all', array('order' => 'id'));
+        foreach ($registrosAlumnos as $key => $value) {
+            $arrResp['items'][] = $value['Alumnos'];
+        }
+        print_r(json_encode($arrResp));
+        exit;
+    }
+
+    /**
+     * @desc Renderizo tutores a la vista principal
+     * @return object    
+     * @access public      
+     */
+    function tutores() {
+        $registrosTutores = $this->Tutores->find('all', array('order' => 'id'));
+        foreach ($registrosTutores as $key => $value) {
+            $arrResp['items'][] = $value['Tutores'];
+        }
+        print_r(json_encode($arrResp));
+        exit;
+    }
+
+    /**
+     * @desc Renderizo dpa a la vista principal
+     * @return object    
+     * @access public      
+     */
+    function dpa() {
+        $registrosDPA = $this->DPA->find('all', array('order' => 'id'));
+        foreach ($registrosDPA as $key => $value) {
+            $arrResp['items'][] = $value['DPA'];
+        }
+        print_r(json_encode($arrResp));
+        exit;
+    }
+
+    function dpa_crear() {
+        $datos = $_POST;
+        $arrDatos = array(
+            'nombre' => $datos['nombre'], //tomamos el campo directamiente de los datos transferidos atraves el post 
+            'primer_apellido' => $datos['primer_apellido'], //es importante que el NAME del campo sea EXACTAMENTE IGUAL al que tomaremos del POST
+            'segundo_apellido' => $datos['segundo_apellido']
+        );
+        //Aqui guardamos el renglon
+        $this->DPA->create(); //creamos un regnlon vacio para
+        $this->DPA->save($arrDatos); //posteriormente guardar ahi los datos del array que generamos
+
+        $respuesta['success'] = true;
+        $respuesta['msg'] = "Datos guardados";
         print_r(json_encode($respuesta));
-      exit;          
-   }
-   function alta_dpa(){//funcion INDISPENSABLE que llama a la vista que genera la ventana para alta del dpa
+        exit;
+    }
 
-   }     
+    function alta_dpa() {//funcion INDISPENSABLE que llama a la vista que genera la ventana para alta del dpa
+    }
 
 }
 
